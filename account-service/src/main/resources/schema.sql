24f785caa880ed66889ffc665c99e81ecbcbd1a2
ckswls56 <skxkswls@gmail.com>
2024-11-02 17:50:10 +0900

    :sparkles: sequence sql 추가

--- account-service/src/main/resources/schema.sql
+++ account-service/src/main/resources/schema.sql
@@ -1,1 +1,7 @@
 create table accounts (id bigint not null, balance integer, customer_id bigint, primary key (id));
+
+CREATE TABLE hibernate_sequence (
+                                    next_val BIGINT NOT NULL
+);
+
+INSERT INTO hibernate_sequence VALUES (1);
